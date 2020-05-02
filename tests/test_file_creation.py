356e00c2fab3012a544d714659b685c0a48f141c
Topolun <vik83tu@yandex.ru>
2020-05-02 17:33:05 +0400

    Tests was changed

--- tests/test_file_creation.py
+++ tests/test_file_creation.py
@@ -1,9 +1,11 @@
 from page_loader import engine
 import tempfile
+import os
 
 
 def test_file_creation():
     with tempfile.TemporaryDirectory() as tmp_dir:
-        engine.create_file('test_file', '1234', tmp_dir)
-        with open('test_file', 'r') as test_file:
+        new_path = os.path.join(os.path.dirname(tmp_dir), 'test_file')
+        engine.create_file('test_file', '1234', os.path.dirname(tmp_dir))
+        with open(new_path, 'r') as test_file:
             assert test_file.read() == '1234'
