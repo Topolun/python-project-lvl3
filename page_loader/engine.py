47543e1e67701f8788e5bf46801f8efc8ab7f690
Topolun <vik83tu@yandex.ru>
2020-05-11 18:51:20 +0400

    Bug fix

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -13,7 +13,10 @@ SELECTORS = {
 
 def create_file(file_name, data='', path=''):
     new_path = os.path.join(path, file_name)
-    with open(new_path, 'w') as new_file:
+    open_method = 'wb'
+    if isinstance(data, str):
+        open_method = 'w'
+    with open(new_path, open_method) as new_file:
         new_file.write(data)
 
 
@@ -25,10 +28,7 @@ def create_dir(dir_path):
 
 def get_name(page_adress, output='file'):
     url = urllib.parse.urlparse(page_adress)
-    url_host = url.hostname
-    if url_host == None:
-        url_host = 'ru.hexlet.io'
-    url_host = url_host.replace('.', '-')
+    url_host = url.hostname.replace('.', '-')
     url_path = url.path.replace('/', '-')
     ext = os.path.splitext(url_path)[1]
     end = '.html'
@@ -37,13 +37,12 @@ def get_name(page_adress, output='file'):
     elif ext is not '':
         end = ''
     name = '{}{}{}'.format(url_host, url_path, end)
+    print('Name: ---- ', name)
     return name
 
 
-def change_path_to_local(path, local_path):
-    url = urllib.parse.urlparse(path)
-    name = get_name(url.path)
-    return '{}/{}'.format(local_path, name)
+def change_path_to_local(file_name, local_path):
+    return '{}/{}'.format(local_path, file_name)
 
 
 def path_normalize_for_download(path, page_adress):
@@ -57,18 +56,19 @@ def content_filter(tag):
 
 def run(data, output):
     page_adress, page_data = data
-    file_name = get_name(page_adress)
-    create_file(file_name, page_data.text, output)
     dir_name = get_name(page_adress, output='dir')
     dir_path = os.path.join(output, dir_name)
     create_dir(dir_path)
     soup = BeautifulSoup(page_data.content)
     tags = soup.find_all(content_filter)
     for tag in tags:
+        print('NEW TAG', tag.name)
         attr = SELECTORS.get(tag.name)
         normalized_path = path_normalize_for_download(tag[attr], page_adress)
-        create_file(get_name(normalized_path), requests.get(normalized_path).text, dir_path)
-        tag[attr] = change_path_to_local(normalized_path, dir_path)
+        print('NORMALIZED PATH: ---------', normalized_path)
+        file_name = get_name(normalized_path)
+        create_file(file_name, requests.get(normalized_path).content, dir_path)
+        tag[attr] = change_path_to_local(file_name, dir_path)
         print(tag[attr])
     file_name = get_name(page_adress)
     create_file(file_name, str(soup), output)
